36a9a60e1bc1311f277dd2d0d997ff06bc3e7881
radriann21 <adrianrangelargu@gmail.com>
2025-01-12 20:02:45 -0400

    initialize project with React, TypeScript, Vite, and Tailwind CSS setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1 className="font-customLight text-3xl text-white">hello world</h1>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          'dark-blue': 'hsl(209, 23%, 22%)',
+          'very-dark-blue': 'hsl(207, 26%, 17%)',
+        },
+        light: {
+          'very-dark-blue': 'hsl(200, 15%, 8%)',
+          'dark-gray': 'hsl(0, 0%, 52%)',
+          'very-light-gray': 'hsl(0, 0%, 98%)'
+        }
+      },
+      fontFamily: {
+        customLight: ['Light'],
+        customSemiBold: ['SemiBold'],
+        customBold: ['Bold']
+      }
+    },
+  },
+  plugins: [require('daisyui')],
+}
